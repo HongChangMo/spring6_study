5ce743bb3a44f989a287fdfa98d02b1d23de1cca
HongChangMo <hcm7402@naver.com>
2025-01-05 23:28:17 +0900

    first commit

---
+++ src/main/java/tobyspring/hellospring/Payment.java
@@ -0,0 +1,59 @@
+package tobyspring.hellospring;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Payment {
+    private Long orderId;
+    private String currency;
+    // BigDecimal을 사용하는 이유: 돈에 대한 계산은 정확한 계산이 필요하기에 사용
+    private BigDecimal foreignCurrencyAmount;
+    private BigDecimal exRate;
+    private BigDecimal convertedAmount;
+    private LocalDateTime validUntil;
+
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate, BigDecimal convertedAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exRate = exRate;
+        this.convertedAmount = convertedAmount;
+        this.validUntil = validUntil;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getForeignCurrencyAmount() {
+        return foreignCurrencyAmount;
+    }
+
+    public BigDecimal getExRate() {
+        return exRate;
+    }
+
+    public BigDecimal getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public LocalDateTime getValidUntil() {
+        return validUntil;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "orderId=" + orderId +
+                ", currency='" + currency + '\'' +
+                ", foreignCurrencyAmount=" + foreignCurrencyAmount +
+                ", exRate=" + exRate +
+                ", convertedAmount=" + convertedAmount +
+                ", validUntil=" + validUntil +
+                '}';
+    }
+}
